8be9a3f18d52140a177e5567ae38026871d8f81f
Ritter <123326885+RitterEagle@users.noreply.github.com>
2023-07-16 01:41:32 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,19 +1,55 @@
-function calculate() {
-  var height = document.getElementById("height").value / 100;
-  var weight = document.getElementById("weight").value;
+class IMC {
+  constructor() {
+    this.heightInput = document.getElementById("height");
+    this.weightInput = document.getElementById("weight");
+    this.resultArea = document.getElementById("text_area");
+    this.calculateButton = document.querySelector("button");
 
-  var imc = weight / height ** 2;
-  var text=""
-  if (imc < 18.5) {
-    text="Você está magro"
-  } else if (imc < 24.9) {
-    text="Você está normal"
-  } else if (imc < 29.9) {
-    text="Você está com sobrepeso"
-  } else if (imc < 39.9) {
-    text="Você está com obesidade"
-  } else if (imc > 39.9) {
-    text="Você está com obesidade mórbida"
+    this.calculateButton.addEventListener("click", this.calculate.bind(this));
+
+    this.loadSessionData();
   }
-  document.getElementById("text_area").innerText=text
+
+  calculate() {
+    var height = this.heightInput.value / 100;
+    var weight = this.weightInput.value;
+
+    var imc = weight / (height ** 2);
+    var text = "";
+
+    if (imc < 18.5) {
+      text = "Você está magro";
+    } else if (imc < 24.9) {
+      text = "Você está normal";
+    } else if (imc < 29.9) {
+      text = "Você está com sobrepeso";
+    } else if (imc < 39.9) {
+      text = "Você está com obesidade";
+    } else if (imc > 39.9) {
+      text = "Você está com obesidade mórbida";
+    }
+
+    this.displayResult(text);
+    this.saveSessionData(height, weight, text);
+  }
+
+  displayResult(text) {
+    this.resultArea.value = text;
+  }
+
+  saveSessionData(height, weight, text) {
+    sessionStorage.setItem("imcData", JSON.stringify({ height, weight, text }));
+  }
+
+  loadSessionData() {
+    const imcData = sessionStorage.getItem("imcData");
+    if (imcData) {
+      const { height, weight, text } = JSON.parse(imcData);
+      this.heightInput.value = height;
+      this.weightInput.value = weight;
+      this.displayResult(text);
+    }
+  }
 }
+
+const imcCalculator = new IMC();
